4c002ea90c7d6a0ab8a147a19260b4967f3c3145
Norbert Fes Geisler <norbert.j.geisler@gmail.com>
2020-10-27 13:26:02 -0700

    Exercises Completed

---
+++ Dissapearing_Ninja/Dissapearing_Ninja.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#pic1").click(function(){
+        $("#pic1").hide();
+    })
+    $("#pic2").click(function(){
+        $("#pic2").hide();
+    })
+    $("#pic3").click(function(){
+        $("#pic3").hide();
+    })
+    $("#pic4").click(function(){
+        $("#pic4").hide();
+    })
+    $("#pic5").click(function(){
+        $("#pic5").hide();
+    })
+    $("#pic6").click(function(){
+        $("#pic6").hide();
+    })
+    $("#pic7").click(function(){
+        $("#pic7").hide();
+    })
+    $("#pic8").click(function(){
+        $("#pic8").hide();
+    })
+    $("#reset").click(function(){
+        $("#pic1").show();
+        $("#pic2").show();
+        $("#pic3").show();
+        $("#pic4").show();
+        $("#pic5").show();
+        $("#pic6").show();
+        $("#pic7").show();
+        $("#pic8").show();
+    })
+})
